ae20808838a6cd3265c1075262981c2d4b619900
Tom Postler <tom@postler.me>
2018-11-11 20:35:26 -0600

    keep alive

---
+++ src/slack-functions/Functions.KeepAlive.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace slack_functions
+{
+    public static class Functions_KeepAlive
+    {
+        public static class Timer
+        {
+            [FunctionName(nameof(KeepAliveEvenings))]
+            public static void KeepAliveEvenings(
+                [TimerTrigger("0 * 17-22 * * *")]TimerInfo timer,
+                ILogger logger)
+            {
+                logger.LogInformation("IT'S {0} AND ALL IS WELL.", DateTimeOffset.Now);
+            }
+
+            [FunctionName(nameof(KeepAliveWeekendDay))]
+            public static void KeepAliveWeekendDay(
+                [TimerTrigger("0 * 11-17 * * Sat,Sun")]TimerInfo timer,
+                ILogger logger)
+            {
+                logger.LogInformation("IT'S {0} AND ALL IS WELL.", DateTimeOffset.Now);
+            }
+        }
+    }
+}
